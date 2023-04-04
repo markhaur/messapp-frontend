3238eaf46ac3705d178e48e13dbee892934d1ab1
Faisal Nisar <fnisar394@protonmail.com>
2023-04-04 14:18:30 +0500

    added login screen and router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignInSide from './SignInSide';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<SignInSide />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
